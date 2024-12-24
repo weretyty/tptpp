d3d3fe2a391d824eececb94d63e1f410916b8c83
Awoms Kelechi <awomskelechi@gmail.com>
2024-12-24 21:40:35 +0100

    updates

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    < div className="p-2 grid gap-2">
+      <div className="bg-rose-400 p-2 h-16 rounded">
+        <p className="text-white">Rose tinted</p>
+      </div>
+      <div className="bg-sky-400 p-2 h-16 rounded">
+        <p className="text-white text-sm">Blue jasmine</p>
+      </div>
+      <div className="bg-green-400 p-2 h-16 rounded">
+        <p className="text-white text-xs">green grass</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
