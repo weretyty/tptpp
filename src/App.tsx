7c48b304ade1140b5e2039de71fe968eb3bb2387
Awoms Kelechi <awomskelechi@gmail.com>
2024-12-25 06:59:54 +0100

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,54 @@
+import { FaCircleUser } from "react-icons/fa6";
+import { PiCurrencyDollarSimpleFill } from "react-icons/pi";
+import { PiKeyReturnFill } from "react-icons/pi";
+import { GrMoney } from "react-icons/gr";
+
+
 function App() {
   return (
-    < div className="p-2 grid gap-2">
-      <div className="bg-rose-400 p-2 h-16 rounded">
-        <p className="text-white">Rose tinted</p>
+    <div className=" bg-neutral-100 h-screen">
+      <div className="flex justify-between h-[60px]  items-center shadow-sm px-6 bg-white">
+        <h1 className="text-base font-semibold text-slate-500">INVESTMENT PORTFOLIO</h1>
+        <div className="flex items-center gap-1">
+          <FaCircleUser className="text-rose-400 text-xl" />
+
+          <p className="text-sm font-semibold text-rose-400">Lilian</p>
+        </div>
       </div>
-      <div className="bg-sky-400 p-2 h-16 rounded">
-        <p className="text-white text-sm">Blue jasmine</p>
-      </div>
-      <div className="bg-green-400 p-2 h-16 rounded">
-        <p className="text-white text-xs">green grass</p>
-      </div>
-      <div className="bg-purple-400 p-2 h-16 rounded">
-        <p className="text-white text-xs">green grass</p>
+
+      {/* Cards */}
+      <div className="flex mt-8 flex-col gap-6 bg-neutral-100 items-center justify-center">
+        <div className="bg-white p-2 h-[130px] w-[90%] shadow-sm border-b-2 border-white rounded border-b-blue-400">
+          <div className="flex flex-col gap-2 w-[60%] mx-auto items-center">
+            <div className="flex gap-2 items-center">
+              <GrMoney className="text-[30px] text-blue-400"/>
+              <p className="text-slate-400 text-lg">Total Invested</p>
+            </div>
+            <div className="text-3xl text-slate-500">$678,375</div>
+          </div>
+
+        </div>
+        <div className="bg-white p-2 h-[130px] w-[90%] shadow-sm border-b-2 border-white rounded border-b-green-400">
+          <div className="flex flex-col gap-2 w-[60%] mx-auto items-center">
+            <div className="flex gap-1 items-center">
+              <PiCurrencyDollarSimpleFill className="text-[40px] text-green-400"/>
+              <p className="text-slate-400 text-lg">Total Earnings</p>
+            </div>
+            <div className="text-3xl text-slate-500">$1,356,750</div>
+          </div>
+
+        </div>
+        <div className="bg-white p-2 h-[130px] w-[90%] shadow-sm border-b-2 border-white rounded border-b-purple-400">
+          <div className="flex flex-col gap-2 w-[60%] mx-auto items-center">
+            <div className="flex gap-1 items-center">
+              <PiKeyReturnFill className="text-[40px] text-purple-400"/>
+              <p className="text-slate-400 text-lg">Rate of return</p>
+            </div>
+            <div className="text-3xl text-slate-500">+100.00%</div>
+          </div>
+
+        </div>
+        
       </div>
     </div>
   );
