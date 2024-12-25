c1db5cf7ed14eb3b69a626a8dbbe5ed78a6c8f61
Awoms Kelechi <awomskelechi@gmail.com>
2024-12-25 14:40:22 +0100

    made update

--- src/components/Trx.tsx
+++ src/components/Trx.tsx
@@ -22,57 +22,85 @@ const Trx = () => {
         {/* Card 1 */}
         <div className="bg-white flex justify-between items-center p-4 h-[100px] w-full mx-auto shadow-sm rounded">
           <div className="flex items-center gap-3">
-            <RiArrowDownCircleFill className="text-3xl text-red-400"/>
+            <RiArrowDownCircleFill className="text-4xl text-red-400"/>
             <div className="flex flex-col">
-              <p className="font-semibold text-sm text-slate-900">Sent</p>
-              <p className="text-xs text-neutral-400">2 days ago</p>
+              <p className="font-semibold text-slate-900">Sent</p>
+              <p className="text-sm text-neutral-400">2 days ago</p>
             </div>
           </div>
           <div className="flex flex-col justify-center">
-            <p className="text-red-400 text-sm font-medium">+0.00102000 BTC</p>
-            <p className="text-xs font-medium text-slate-900">234 USD</p>
+            <p className="text-red-400 text font-medium">-0.02199 BTC</p>
+            <p className="text-sm font-medium text-slate-900">2160 USD</p>
           </div>
         </div>
         {/* Card 2 */}
         <div className="bg-white mt-3 flex justify-between items-center p-4 h-[100px] w-full mx-auto shadow-sm rounded">
           <div className="flex items-center gap-3">
-            <RiArrowUpCircleFill className="text-3xl text-green-400"/>
+            <RiArrowUpCircleFill className="text-4xl text-green-400"/>
             <div className="flex flex-col">
-              <p className="font-semibold text-sm text-slate-900">Recieved</p>
-              <p className="text-xs text-neutral-400">3 days ago</p>
+              <p className="font-semibold text-slate-900">Recieved</p>
+              <p className="text-sm text-neutral-400">3 days ago</p>
             </div>
           </div>
           <div className="flex flex-col justify-center">
-            <p className="text-green-400 text-sm font-medium">+0.00102000 BTC</p>
-            <p className="text-xs font-medium text-slate-900">234 USD</p>
+            <p className="text-green-400 font-medium">+0.01221 BTC</p>
+            <p className="text-sm font-medium text-slate-900">1200 USD</p>
           </div>
         </div>
         {/* Card 3 */}
         <div className="bg-white mt-3 flex justify-between items-center p-4 h-[100px] w-full mx-auto shadow-sm rounded">
           <div className="flex items-center gap-3">
-            <RiArrowDownCircleFill className="text-3xl text-red-400"/>
+            <RiArrowDownCircleFill className="text-4xl text-red-400"/>
             <div className="flex flex-col">
-              <p className="font-semibold text-sm text-slate-900">Sent</p>
-              <p className="text-xs text-neutral-400">5 days ago</p>
+              <p className="font-semibold text-slate-900">Sent</p>
+              <p className="text-sm text-neutral-400">5 days ago</p>
             </div>
           </div>
           <div className="flex flex-col justify-center">
-            <p className="text-red-400 text-sm font-medium">+0.00102000 BTC</p>
-            <p className="text-xs font-medium text-slate-900">234 USD</p>
+            <p className="text-red-400 font-medium">-0.16486 BTC</p>
+            <p className="text-sm font-medium text-slate-900">16,200 USD</p>
           </div>
         </div>
         {/* Card 4 */}
         <div className="bg-white mt-3 flex justify-between items-center p-4 h-[100px] w-full mx-auto shadow-sm rounded">
           <div className="flex items-center gap-3">
-            <RiArrowUpCircleFill className="text-3xl text-green-400"/>
+            <RiArrowUpCircleFill className="text-4xl text-green-400"/>
             <div className="flex flex-col">
-              <p className="font-semibold text-sm text-slate-900">Recieved</p>
-              <p className="text-xs text-neutral-400"> 6 days ago</p>
+              <p className="font-semibold text-slate-900">Recieved</p>
+              <p className="text-sm text-neutral-400"> 6 days ago</p>
             </div>
           </div>
           <div className="flex flex-col justify-center">
-            <p className="text-green-400 text-sm font-medium">+0.00102000 BTC</p>
-            <p className="text-xs font-medium text-slate-900">234 USD</p>
+            <p className="text-green-400 font-medium">+0.0917 BTC</p>
+            <p className="text-sm font-medium text-slate-900">9000 USD</p>
+          </div>
+        </div>
+        {/* Card 5 */}
+        <div className="bg-white mt-3 flex justify-between items-center p-4 h-[100px] w-full mx-auto shadow-sm rounded">
+          <div className="flex items-center gap-3">
+            <RiArrowDownCircleFill className="text-4xl text-red-400"/>
+            <div className="flex flex-col">
+              <p className="font-semibold text-slate-900">Sent</p>
+              <p className="text-sm text-neutral-400">6 days ago</p>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center">
+            <p className="text-red-400 font-medium">-0.0632 BTC</p>
+            <p className="text-sm font-medium text-slate-900">6210 USD</p>
+          </div>
+        </div>
+        {/* Card 6 */}
+        <div className="bg-white mt-3 flex justify-between items-center p-4 h-[100px] w-full mx-auto shadow-sm rounded">
+          <div className="flex items-center gap-3">
+            <RiArrowUpCircleFill className="text-4xl text-green-400"/>
+            <div className="flex flex-col">
+              <p className="font-semibold text-slate-900">Recieved</p>
+              <p className="text-sm text-neutral-400"> 7 days ago</p>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center">
+            <p className="text-green-400 font-medium">+0.0351 BTC</p>
+            <p className="text-sm font-medium text-slate-900">3450 USD</p>
           </div>
         </div>
         
